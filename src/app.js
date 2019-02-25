d1e22b89875d6c57196f514cd36190e8665394f4
Nuno Farinha <nadfarinha@gmail.com>
2019-02-25 16:35:44 +0000

    Create form view

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+const FormView = require('./views/form_view.js');
+const PrimeChecker = require('./models/prime_checker.js')
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  const formView = new FormView();
+  formView.bindEvents();
+  
 });
